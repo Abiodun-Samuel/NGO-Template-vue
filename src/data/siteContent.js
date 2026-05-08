6c809c908c3064293d521b3d854c6ce61a40b2ca
Abiodun Samuel O. <69618294+Abiodun-Samuel@users.noreply.github.com>
2026-05-08 10:48:19 +0100

    Add initial content schema and full image inventory

---
+++ src/data/siteContent.js
@@ -0,0 +1,32 @@
+export const siteContent = {
+  meta: {
+    siteName: 'Brand Name',
+    siteTagline: 'Making a Difference Together',
+    siteDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    siteUrl: import.meta.env.VITE_SITE_URL || 'https://example.com',
+    locale: 'en_US',
+    themeColor: '#fafaf9',
+    twitterHandle: '@example',
+  },
+  seo: {
+    home: {},
+    about: {},
+    gallery: {},
+    getInvolved: {},
+    contact: {},
+    latestNews: {},
+    latestNewsDetail: {},
+    notFound: {},
+  },
+  navigation: { links: [] },
+  home: {},
+  about: {},
+  gallery: {},
+  getInvolved: {},
+  contact: {},
+  news: {},
+  footer: {},
+  ui: {},
+}
+
+export default siteContent
